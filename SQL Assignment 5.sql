0eb41e51c4d3a9b0ee5e6744376ae8c07c3e2fca
ROHIT KUMAR <87421917+Rohit-0310@users.noreply.github.com>
2025-02-13 18:17:24 +0530

    update

--- SQL Assignment 5.sql
+++ SQL Assignment 5.sql
@@ -3,3 +3,22 @@ create database RetailSalesData;
 use RetailSalesData;
 
 select * from groceryData;
+
+-- 1. Basic GROUP BY and Aggregation
+-- Find the total sale_amount per category.
+
+select category, sum(sales) as total_sale_amount from groceryData
+group by category;
+
+-- 2. Basic ORDER BY
+-- Retrieve all sales ordered by sale_amount in descending order.
+
+select * from groceryData order by sales desc;
+
+
+-- List all sales in the 'Food Grains' category, sorted in ascending orde by order_id.
+
+
+
+
+
